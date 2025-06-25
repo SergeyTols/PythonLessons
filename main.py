548a42366257fe7031b87bd676c0e1c61a7fd155
Sarge <a@b.ru>
2025-06-25 14:11:21 +0300

    Lesson1_2

--- main.py
+++ main.py
@@ -1,61 +1,9 @@
-# Ctrl + Shift + стрелка вверх/вниз - двигать выделенное
-# Ctrl + Shift + стрелка вправо - выделить слово
-# Ctrl + z - отмена
-# PEP 8     Alt + Ctrl + L
-# Ctrl + /  - закоментить строку (на русском "спрятать")
+hour = 13
 
-# name = 'Сергей'  # str
-# name = input('Как тебя зовут? ')  # str
-# surname = 'Петров'
-# age = 27  # int
-# temper = 13.4  # float (floating point)
-#
-# print('привет,', name, surname, 'тебе', age, 'лет')
-# print('На улице', temper)
+if hour >23:
+    hour = 23
+if hour < 0:
+    hour = 0
 
-# donut = input('Стоимость пончика: ')
-# coffee = input('Стоимость кофе: ')
-#
-# print('С вас', int(coffee) + int(donut), 'руб.')
-
-# donut = int(input('Стоимость пончика: '))
-# coffee = float(input('Стоимость кофе: '))
-#
-# print('С вас', coffee + donut, 'руб.')
-
-number = int(input('Vvedi '))
-print('Число', number, 'в степени 3 будет', number ** 3)
-print('Квадратный корень от', number, 'будет:', number ** (1 / 2))
-
-temp = """
-Вансив
-Авансив
-"""
-
-print(temp)
-
-'''
-== - равно
-!= - не равно
-indent - кочка, отступ
-unindent - возврат отступа
-
-    Правописание if:
-if <условие>:
-    команды
-'''
-
-promt = """Виитязь на распутье
-Налево (L) пойдёшь, волю обретешь...
-Направо (R) пойдёшь, коня потеряешь...
-Прямо (F) пойдёшь, то и сыт..."""
-print(promt)
-choice = input('Куда идём (L, R или F): ')
-if choice == 'L' or choice == 'l':
-    print('Вольный ты')
-elif choice == 'R' or choice == 'r':
-    print('Теперь пешком')
-elif choice == 'F' or choice == 'f':
-    print('теперь сыт!')
-else:
-    print('Выбор не ясен...')
+if hour <= 7 and hour < 12:
+    print()

1d7576f64824b2a922cbab751a95e1546eed3815
Sarge <a@b.ru>
2025-06-26 14:17:02 +0300

    Match Case

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-height = int(input('Введи рост: '))
-
-# while height > 180 or height < 150:
-while not (150 < height < 180):
-    print('не подходит')
-    height = int(input('Введи рост: '))
-
-print(f'{height} подходит')
 

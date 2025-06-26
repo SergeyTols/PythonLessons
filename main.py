9693faaa4bd81c761b05cad1ea79a28cb8f51f40
Sarge <a@b.ru>
2025-06-26 14:13:32 +0300

    loop рост кандидата

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 height = int(input('Введи рост: '))
 
-while height > 180 or height < 150:
-# while 150 > height < 180:
+# while height > 180 or height < 150:
+while not (150 < height < 180):
     print('не подходит')
     height = int(input('Введи рост: '))
 

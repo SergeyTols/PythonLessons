04468ae54fcc626c317008d8098a95f897b3d050
Sarge <a@b.ru>
2025-06-26 14:36:54 +0300

    доп задание. Уточнить %

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-print('Варианты:\n\tL - влево\n\tR -вправо\n\tF - прямо\n\t Q - выход')
+counter = 1
 
-while  True:
-    ch = input('Ваш выбор: ')
-    match ch:
-        case 'L' | 'l' | 'Д' | 'д':
-            print('Свернул вправо')
-
-
-        case 'Q' | '' | '' | '':
-            print()
-            break
-        case _:
+while counter <= 100:
+    if counter % 10 == 3:
+        print(counter, end=', ')
+    counter += 1
 

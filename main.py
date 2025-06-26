a452bb7ea7b441ec4b2f99a865bd9c68bc3fb2c2
Sarge <a@b.ru>
2025-06-26 14:56:25 +0300

    for

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-word = 'поток'
-if 'ток' in word:
-    print('есть')
+# for <переменная> in interable:
+
+# word = 'поток'
+#
+# for ch in word:
+#     print(ch)
+#                  0      3     1
+# итератор range(start, stop, step)
+
+# for i in range(0,3,1):
+#     print(i)
+
+# for _ in range(5):
+#     print("hi")
+
+for i in range(0, 101):
+    if i % 10 == 5:
+        if i == 15:
+            continue
+        print(i)

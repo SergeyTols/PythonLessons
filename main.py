c6f7ed1c78a416b28740a2eadc328e7ddaf41d55
Sarge <a@b.ru>
2025-06-26 14:29:25 +0300

    Match Case

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+print('Варианты:\n\tL - влево\n\tR -вправо\n\tF - прямо\n\t Q - выход')
+
+while  True:
+    ch = input('Ваш выбор: ')
+    match ch:
+        case 'L' | 'l' | 'Д' | 'д':
+            print('Свернул вправо')
+
+
+        case 'Q' | '' | '' | '':
+            print()
+            break
+        case _:
 

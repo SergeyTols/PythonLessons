d19afecfec132101d15b3d4492da2fc203a4fdeb
Sarge <a@b.ru>
2025-06-27 14:59:59 +0300

    Str строки изменение буквы в слове

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-# Строки (immutable, iterable) - неизменяемый тип данных
-#    012345
-s = 'Python'
-# s[3] = 'y' error (immutable)
-# Индекс может быть отрицательным (с конца)
-print(s[0])
+s = 'сабака'
+res = ''
 
+for i in range(len(s)):
+    if i == 1:
+        res += 'о'
+    else:
+        res += s[i]
 
-words = 'язык питон'
-vovel = 0
-
-for ch in words: #
-    if ch in {'а', 'е', 'и', 'о', 'у', 'э', 'ю', 'я', 'ы'}:
-        vovel += 1
-    # if ch in 'аеиоуэюяы':
-    #     vovel += 1
-print(f'гласных в строке "{words}" = {vovel}')
-
-# перебор строки по числовому индексу
-for index in range(len(words)):
-    print(words[index], end=' ')
+print(res)

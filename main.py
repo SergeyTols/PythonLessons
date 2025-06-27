b5a1ca5412da11ce002a2a7958db4939675de71e
Sarge <a@b.ru>
2025-06-27 15:27:41 +0300

    Str Таблица символов

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-s = 'сабака'
-res = ''
+# Таблица символов
+s = '\xB0'
+u = '\u2603'
 
-for i in range(len(s)):
-    if i == 1:
-        res += 'о'
-    else:
-        res += s[i]
+# Две удобные функции
+# ord(символ) - возвращает код символа в Unicode
+# chr(код) - возвращает символ по Unicode-коду
 
-print(res)
+print('25' + s + 'C')
+print(u)
+print({ord('☃')})
+print(chr(9731))
+print(chr(176))  # ASCII и Unicode

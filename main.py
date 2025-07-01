ac14db62282c40c8c9c69d00d2c2e42be7d66f68
Sarge <a@b.ru>
2025-07-01 16:13:47 +0300

    Functions Return value + ДЗ

--- main.py
+++ main.py
@@ -1,47 +1,33 @@
-## Функции
-# Scope (local or global) - область видимости переменной
-# Синтаксис:
-# def <имя функции>([параметры]):
-#     команды
-# Если функция ничего не возвращает - это процедура
-# В функцию передается копия глобальной переменной
-from itertools import count
+# Функции
+# Return Value
+# Чистая ф-ция - ф-ция, не имеющая эффекта на переменные и элементы, не влияет на ход выполнения программы
+# Вычисление квадрата числа
 
-person = 'Пётр'  # (global) глобальная переменная
-count = 0
-
-def greet():
-    print('Привет')
+def square(num):
+    return num ** 2
+    # temp = num ** 2
+    # return temp
 
 
-greet()
-greet()
+
+def even_odd(num):
+    if num % 2 == 0:
+        return 'Четное'
+    return 'Нечетное'
+    # if num % 2 == 0:
+    #     return 'Четное'
+    # else:
+    #     return 'Нечетное'
 
 
-def greet_to_name(name='noneme'):  # функция задана с ПАРАМЕТРОМ   #| (local) локальная переменная
-    print('Привет', name)                                # |
 
+print(even_odd(5))
+t = square(5)
+print(t)
 
-greet_to_name('Вова')  # функция вызвана с АРГУМЕНТОМ
-
-
-greet_to_name()
-
-
-def increment():
-    global count     # доступ к глобальной переменной из функции
-    count += 1
-    print(count)
-
-
-def print_list(array=None):
-    if array is None:
-        array = []
-    for item in array:
-        print(item)
-
-
-increment()
-print_list(['мясо', 'рыба', 'тесто', 'детство'])
-
-
+# ДЗ: Функция: Вывести число словами 56 -> триста пятьдесят шесть.
+# def num_to_word(num):
+#     if str(num) > 2:
+#         return
+#     e = num % 10
+#     le = ['один', 'два']

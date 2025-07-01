f64491442113773420178e6de8e39f6394cf6cb4
Sarge <a@b.ru>
2025-07-01 10:17:44 +0300

    ДЗ просмотреть и использовать

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 ## ДЗ
 # stop_list = [] - Вводить слова, вывести сплитом пронумерованную коллонку слов в алфавит порядке, кроме стопслов из списка
 # убрать слова в конце с помощью множеств(?)
+                #1
+stop_words = ['ну', 'типо', 'короче']
+temp = []
+message = input('Введите сообщение: ')
+lst = message.split()
+for item in lst:
+    if item not in stop_words:
+        temp.append(item)
+res = sorted(temp)
+for a, b in enumerate(res, 1):
+    print(f'{a}. {b}')
 
-words_list = []
-
-while (word := input('Введите слово: ').lower().add(words_list)) != '':
-    pass
+                #2
+stop_words = ['ну', 'типо', 'короче']
+stop_words = set(stop_words)
+message = input('Введите сообщение: ')
+lst = message.split()
+res = sorted(set(lst) - stop_words)
+for a, b in enumerate(res, 1):
+    print(f'{a}. {b}')

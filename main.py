bb34604cd45a5c4dbadd2e4f97650191fddceaa7
Sarge <a@b.ru>
2025-07-01 11:12:13 +0300

    List comprehension (Списочные выражения)

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
-## ДЗ
-# stop_list = [] - Вводить слова, вывести сплитом пронумерованную коллонку слов в алфавит порядке, кроме стопслов из списка
-# убрать слова в конце с помощью множеств(?)
-                #1
-stop_words = ['ну', 'типо', 'короче']
-temp = []
-message = input('Введите сообщение: ')
-lst = message.split()
-for item in lst:
-    if item not in stop_words:
-        temp.append(item)
-res = sorted(temp)
-for a, b in enumerate(res, 1):
-    print(f'{a}. {b}')
+## Списочные выражения (list comprehension)
 
-                #2
-commas = (',', '.', '?', '!', '-')  #  кортеж
-stop_words = ['ну', 'типо', 'короче']
-stop_words = set(stop_words)
-message = input('Введите сообщение: ')
-for z in commas:
-    message = message.replace(z, '')
-lst = message.split()
-res = sorted(set(lst) - stop_words)
-for a, b in enumerate(res, 1):
-    print(f'{a}. {b}')
+squares = []
+for i in range(10):
+    squares.append(i ** 2)
+print(*squares, sep=', ')
+
+#   Список квадратов чисел
+squares = [i ** 2 for i in range(10)]
+print(*squares, sep=', ')
+
+#   Список квадратов четных чисел
+
+#         | что | |     закон      | |   условие  |
+squares = [i ** 2 for i in range(10) if i % 2 == 0]
+print(*squares, sep=', ')
+
+#   произведение i и j
+# for i in range(3):
+#     for j in range(3):
+#        print(i * j)
+print([i * j for i in range(3) for j in range(3)], sep=', ')
+print(*[i * j for i in range(3) for j in range(3)], sep=', ')
+
+#   Строку чисел в список
+n = '500 600 700 800'
+approved = [500, 800]
+print([int(i) for i in n.split()])
+
+#   какие-то действия со списком "а"
+a = [int(i) for i in n.split()]
+print(a)
+print([int(i) for i in n.split() if int(i) in approved])
+
+
+

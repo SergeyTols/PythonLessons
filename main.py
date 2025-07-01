a1841b45adf3c04c6981d16b83ed1a5318706127
Sarge <a@b.ru>
2025-07-01 12:11:56 +0300

    Nested lists (вложенные списки)

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 ## Списочные выражения (list comprehension)
+## Вложенные списки
+# table = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9],
+# ]
+N = 3
+table = [[1] * N for _ in range(N)]
+# обход 2-мерного списка (матрицы)
+count = 1
+for row in range(len(table)):
+    for col in range(len(table[row])):
+        table[row][col] = count
+        # print(table[row][col])
+        count += 1
 
-text = 'Списочные выражения применяются для эффективности кода'
-print([i for i in text.split()[2::3]])
-
-
+print(table)

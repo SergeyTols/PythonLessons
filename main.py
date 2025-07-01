837ff1cc8a731b0049248621e8961011c499e23d
Sarge <a@b.ru>
2025-07-01 12:25:28 +0300

    (прога, ОБДУМАТЬ) Вложенные списки

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-## Списочные выражения (list comprehension)
-## Вложенные списки
-# table = [
-#     [1, 2, 3],
-#     [4, 5, 6],
-#     [7, 8, 9],
-# ]
-N = 3
-table = [[1] * N for _ in range(N)]
-# обход 2-мерного списка (матрицы)
-count = 1
-for row in range(len(table)):
-    for col in range(len(table[row])):
-        table[row][col] = count
-        # print(table[row][col])
-        count += 1
+# ## Списочные выражения (list comprehension)
+# ## Вложенные списки
+#
+matrix = []
 
-print(table)
+start = 1
+N = 4
+
+for i in range(N):
+    table = []
+    for j in range(start, start + N):
+        table.append(j)
+    matrix.append(table)
+    table = []
+    start += N
+
+print(*matrix)
+
+# N = 3
+# matrix = [[i + j for j in range(N)] for i in range(1, 9, 3)]
+# print(matrix)

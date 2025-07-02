fd922f54a9434dd30f17da11379954443b183fb8
Sarge <a@b.ru>
2025-07-02 10:30:43 +0300

    Not TODO

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
-# Функции
-# Функция с аннотацией
-num_to_str = {
+# Области видимости
+# Пример как делать не надо
 
-}
-
-                      #any
-def number_to_words(n: int) -> str:  # При использовании ретерн
-    """
-    Функция... (тут описание)
-    :param n: двузначное число
-    :return: это число словами
-    """
-    if len(str(n)) > 2:
-        return  'Введите двузначное число'
-    if len(str(n)) == 1 or n in num_to_str:
-        return num_to_str[int(n)]
-    return num_to_str[int(str(n)[0] + '0')] + ' ' + num_to_str[int(str(n)[1])]
+a = [1, 2]
 
 
-print(number_to_words(13))
+def change_array():
+    a[0] = 0
+
+
+change_array()
+print(a)

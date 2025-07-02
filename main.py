f59cbc9127384ac111ea09813d6bb99f2c32bb64
Sarge <a@b.ru>
2025-07-02 16:08:11 +0300

    Filter

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-#       Позиционные | Именованные
-def print_any(*args, **kwargs):
-    for i in args:
-        print(i)
-    for k, v in kwargs.items():
-        print(k, '=', v)
+#   Функция, как объект
+#   передается в другие функции(функции высшего порядка)
+печатник = print
+печатник('Привет мир')
+
+#   Функция критерия отбора элементов списка
+#   Критерий: длинна слова
+def is_longer_six(word):
+    # if len(word) > 6:
+    #     return word
+    return len(word) > 6
 
 
-print_any('Дмитрий', 'Колесов', city='Москва', age=27)
+def first_c(word):
+    return word[0] == 'с'
 
 
-def profile(name, surname, city, *children, **additional):
-    print(f'Имя: {name}')
-    print(f'Фамилия: {surname}')
-    print(f'Из города: {city}')
-    if len(children) > 0:
-        print('Дети:',', '.join(children))
-    if 'hobbie' in additional:
-        print('Хобби:', ', '.join(additional['hobbie']))
-    # print(additional)
 
+words = ['В', 'этом', 'списке', 'останутся', 'слова', 'длинна', 'которых',
+         'больше', 'шести']
 
-profile('Дмитрий', 'Колесов', 'Волгоград', 'Мария', 'Петр', hobbie=['Филателия', 'Шахматы'])
+result = list(filter(is_longer_six, words))
+print(result)
+
+for word1 in filter(is_longer_six, words):
+    print(word1)
+
+result2 = list(filter(first_c, words))
+print(result2)

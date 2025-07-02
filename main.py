1e3b9e626e7365d25eaf4cd672d0c9baa7742963
Sarge <a@b.ru>
2025-07-02 14:29:27 +0300

    Ф-ция с переменным числом аргументов

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 #  Функция с переменным числом аргументов
 
-def multy(*args):
-    print(len(args))  # подсчет числа аргументов
-    print(args)  # вывод по индексу или перебором в цикле
+def multy(*args, first=0):
+    # print(len(args))  # подсчет числа аргументов
+    # print(args)  # вывод по индексу или перебором в цикле
     # if len(args) == 0:
     #     return 0
     if not args:
-        return 0
-    result = 1
+        return first
+    result = first
     for arg in args:
         result *= arg
     return result
 
 
 # multy(1, 2)
-print(multy(5.4, 3.2, 4.7))
+print(multy(2, 3, 4, first=5))
+
+
+def fio(name, surname):
+    return f'{name} {surname}'
+
+
+print(fio(name='Остап', surname='Бендер'))
+
+
+

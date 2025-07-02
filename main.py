3afe20feeba17fee6f3fa8a78090fa5b2b6057c7
Sarge <a@b.ru>
2025-07-02 12:42:08 +0300

    Функции. Оператор is in practice

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-#   Оператор is: a is b -> когда a и b - один и тот же объект
+#   Оператор is применяем на практике
 
-my_refregirator = ['колбаса', 'сыр', 'масло']
-# his_refregirator = ['колбаса', 'сыр', 'масло']
+def print_array(array: list, start: int = None):
+    if start > len(array):
+        return
+    if start is None:
+        for i in array:
+            print(i)
+    else:
+        for i in range(start, len(array)):
+            print(array[i])
 
-his_refregirator = my_refregirator
-my_refregirator += ['мясо']
-his_refregirator += ['хлеб']
-print(my_refregirator == his_refregirator)
-print(id(my_refregirator) == id(his_refregirator))
-print(his_refregirator)
-print(my_refregirator)
-temp = None
-print(type(temp))
-print(temp is None)
+
+a = [1, 2, 3]
+print_array(a, 1)
+
+# my_refregirator = ['колбаса', 'сыр', 'масло']
+# # his_refregirator = ['колбаса', 'сыр', 'масло']
+#
+# his_refregirator = my_refregirator
+# my_refregirator += ['мясо']
+# his_refregirator += ['хлеб']
+# print(my_refregirator == his_refregirator)
+# print(id(my_refregirator) == id(his_refregirator))
+# print(his_refregirator)
+# print(my_refregirator)
+# temp = None
+# print(type(temp))
+# print(temp is None)
 
 #   Словарь также изменяем, как и множество со списком
 # d = {'a': 1}

22518d9a29da97320c8567b38221095543271b5c
Sarge <a@b.ru>
2025-07-02 12:29:24 +0300

    Функции. Оператор is

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-#
+#   Оператор is: a is b -> когда a и b - один и тот же объект
 
-def print_goodbye(arg):
-    print('Goodbye', end=' ')
+my_refregirator = ['колбаса', 'сыр', 'масло']
+# his_refregirator = ['колбаса', 'сыр', 'масло']
+
+his_refregirator = my_refregirator
+my_refregirator += ['мясо']
+his_refregirator += ['хлеб']
+print(my_refregirator == his_refregirator)
+print(id(my_refregirator) == id(his_refregirator))
+print(his_refregirator)
+print(my_refregirator)
+temp = None
+print(type(temp))
+print(temp is None)
+
+#   Словарь также изменяем, как и множество со списком
+# d = {'a': 1}
+# print(id(d))
+# d['a'] += 1
+# print(id(d))
+
+# a = [0]
+# print(id(a))
+# a [0] += 1
+# print(id(a))
+
+# a = 1
+# print(id(a))
+# a += 1
+# print(id(a))
 
 
-def print_cruel(arg):
-    print('cruel', end=' ')
-
-
-def print_world(arg):
-    print('world', end=' ')
-
-
-def main():
-    print_goodbye(1)
-    print_cruel(1)
-    print_world(2)
-
-
-main()

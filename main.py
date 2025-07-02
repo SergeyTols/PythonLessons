5611b1866c0b61c458a78e5203b6b85c65d39a7d
Sarge <a@b.ru>
2025-07-02 16:21:09 +0300

    Map

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
 #   Функция, как объект
 #   передается в другие функции(функции высшего порядка)
-печатник = print
-печатник('Привет мир')
 
-#   Функция критерия отбора элементов списка
-#   Критерий: длинна слова
-def is_longer_six(word):
-    # if len(word) > 6:
-    #     return word
-    return len(word) > 6
+def square(num):
+    # return num ** 2
+    return str(num ** 2)
 
 
-def first_c(word):
-    return word[0] == 'с'
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+squares = map(square, nums)
+print(list(squares))
+# print(squares)
+
+# С помощью map сделать строками, с помощью join слить в 1
 
 
 
-words = ['В', 'этом', 'списке', 'останутся', 'слова', 'длинна', 'которых',
-         'больше', 'шести']
+# words = ['В', 'этом', 'списке', 'останутся', 'слова', 'длинна', 'которых',
+#          'больше', 'шести']
+#
+# result = list(filter(is_longer_six, words))
+# print(result)
 
-result = list(filter(is_longer_six, words))
-print(result)
-
-for word1 in filter(is_longer_six, words):
-    print(word1)
-
-result2 = list(filter(first_c, words))
-print(result2)

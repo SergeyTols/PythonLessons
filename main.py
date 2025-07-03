d4555f2510effb6a7c92c04ff22572bface8caff
Sarge <a@b.ru>
2025-07-03 12:42:48 +0300

    all and any

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+# Анонимные ф-ции
+# Проверка коллекции
 
+# any - любой элемент коллекции вернул True
+# all - все эл-ты коллекции вернули True
 
-numbers = range(1,11)
-squar = {n: n ** 2 for n in range(1, 11) if n % 2 == 0}
-print(squar)
+print(all([1, 2, 3])) # все эл-ты ненулевые
+print(all([1, 2, 0])) # один элемент нулевой
+print(all([]))
 
-source_dict = {
-    'x': 1,
-    'y': 2,
-    'z': 3,
-}
-dest_dict = {k: v * 2 for k, v in source_dict.items()}
-print(dest_dict)
+words = 'один два три'.split()
+
+# list_for_analize = list(map(lambda x: len(x) > 2, words))
+print(any(list(map(lambda x: len(x) > 5, words))))
+
+# fruits = ['ананас', 'банан', 'ежевика', 'арбуз', 'малина']
+#
+# # print(sorted(fruits, key=lambda s: (len(s), s[-1])))
+#
+# goods = [
+#     ['Утюг', 1000, 2],
+#     ['Фен', 1000, 5],
+#     ['Телевизор', 8000, 3],
+# ]
+#
+# print(sorted(goods, key=lambda s: (s[1], s[2], s[0])))  # Сортировка по цене, кол-ву, имени
+

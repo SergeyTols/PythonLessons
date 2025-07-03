e5edb9bc80e0cbe537305f3311f0e5bf31c4341f
Sarge <a@b.ru>
2025-07-03 14:40:22 +0300

    Потоковый ввод. Stdin Sample

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 # Анонимные ф-ции
 # Потоковый ввод sys.stdin (Ctrl + D)
 import sys
+from idlelib.tooltip import OnHoverTooltipBase
+from operator import index
 
 # data = sys.stdin.readlines()
 #
 # data = [d.strip('\n') for d in data]
 
 data = [d.strip('\n') for d in sys.stdin.readlines()]
+temp = []  # индекс строки в data и число слов в виде кортежей
+for i, s in enumerate(data):
+    temp.append((i, len(s.split())))
+temp.sort(key=lambda x: x[1])
+index = temp[0][0]
+res = sorted(data[index].split())
+print(*res, sep='-')
 
-print(data)
+# раз два три
+# елочка гори
 
 # for line in sys.stdin:
 #     print(line)
@@ -36,4 +46,3 @@ print(data)
 # ]
 #
 # print(sorted(goods, key=lambda s: (s[1], s[2], s[0])))  # Сортировка по цене, кол-ву, имени
-

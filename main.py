0bbe904ddc59a0a9dc6b348c67df93a4751dc628
Sarge <a@b.ru>
2025-07-04 12:46:27 +0300

    Модуль datetime

--- main.py
+++ main.py
@@ -1,54 +1,26 @@
 # Встроеннные библиотеки
 # PyPI - Python Package Index (pypi.org)
 # sample (в отличие от choice) выбирает уникальные (без повторов)
-# s
-import random as r
-# r.seed()
-# print(r.random())
 
-abc = 'wqertyuiopasdfghjklzxcvbnm'
-# lst = list(abc) + ['1', '2'] + ['#', '$']
-N = 8
-num = '1234567890'
-spec = '!@#$%'
+import datetime as dt
 
-abc = list(abc)
-num = list(num)
-spec = list(spec)
+my_time = dt.time(15, 27, 32)
+print(my_time)
+my_day = dt.date(2025, 7, 4)
+print(my_day)
+my_date_time = dt.datetime.combine(my_day, my_time)
+print(my_date_time)
 
-temp = abc[:N - 3]
-temp.append(r.choice(abc).upper())
-temp.append(r.choice(num))
-temp.append(r.choice(spec))
-r.shuffle(temp)
-res = ''.join(temp)
+date1 = dt.date(2025, 6 , 15)
+date2 = dt.date(2025, 7 , 3)
+delta = date1 - date2
 
-print(res)
+print(delta)
 
-# zara = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
+# # print(dt.datetime.now().time())
+# time = dt.datetime.now()
+# ftime = time.strftime('%d-%m-%Y') # %y - будет только 25
+# ftime1 = time.strftime('%H:%M:%S')
 #
-# for _ in range(10):
-#     print(r.choice(zara), r.choice(zara))
-#
-# d = {'a': 1,
-#      'b': 2,
-#      'c': 3,
-# }
-# keys = list(d.keys())
-#
-# key = r.choice(keys)
-# print(d[key])
-
-# lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-#
-# for _ in range(5):
-#     print(r.sample(lst, k=5))
-
-# print(r.choice(['орёл', 'решка']))
-# print(r.choice('орёл'))
-
-# print(r.choice(lst))
-
-# for _ in range(10):
-#     r.randint(0, 10)
-#     print(r.randrange(0, 10, 2))
+# print(ftime)
+# print(ftime1)

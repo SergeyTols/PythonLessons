7764ec2da5fb04c695a3968b72614a94726d3631
Sarge <a@b.ru>
2025-07-04 12:56:16 +0300

    Модуль pprint уточнить

--- main.py
+++ main.py
@@ -2,25 +2,12 @@
 # PyPI - Python Package Index (pypi.org)
 # sample (в отличие от choice) выбирает уникальные (без повторов)
 
-import datetime as dt
+from pprint import pprint
 
-my_time = dt.time(15, 27, 32)
-print(my_time)
-my_day = dt.date(2025, 7, 4)
-print(my_day)
-my_date_time = dt.datetime.combine(my_day, my_time)
-print(my_date_time)
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+]
 
-date1 = dt.date(2025, 6 , 15)
-date2 = dt.date(2025, 7 , 3)
-delta = date1 - date2
-
-print(delta)
-
-# # print(dt.datetime.now().time())
-# time = dt.datetime.now()
-# ftime = time.strftime('%d-%m-%Y') # %y - будет только 25
-# ftime1 = time.strftime('%H:%M:%S')
-#
-# print(ftime)
-# print(ftime1)
+pprint(matrix)

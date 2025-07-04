9def8d0fa268f8b75e221011437f3f2083f6e2d6
Sarge <a@b.ru>
2025-07-04 10:25:32 +0300

    ДЗ решение

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ if rem:
     strings = strings[:length - rem]
 
 for x in range(0, length - rem, 3):
-    summ = sum([len(a)] for a in strings[x:x + 3])
+    summ = sum(len(a) for a in strings[x:x + 3])
     result = []
-    for s in strings[x:x +3]:
+    for s in strings[x:x + 3]:
         temp = s.lower().split()
         result += filter(lambda a: len(a) % 2 == summ % 2, temp)
-    result = sorted(set(map(lambda b: b.capitalize(), result))) [:5]
+    result = sorted(set(map(lambda b: b.capitalize(), result)))[:5]
     print(*result, sep='. ')

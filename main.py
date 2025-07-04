ea3a2fdce535f2ad25cba72c31f3d457c2634bd8
Sarge <sarge2033@gmail.com>
2025-07-05 02:33:31 +0300

    H/W done?

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-# Вешние библиотеки
-# Графика
-# PIL - Pythom Imagine Library (pillow - подушка)
-# Команды в терминал:
-# pip install pillow
-# pip freeze > requirements.txt - создание файла зависимости
-# pip install -r requirements.txt - установка списка библиотек
-# PyPI - Python Package Index (pypi.org)
+
 from PIL import Image, ImageDraw
+from PIL.ImageFont import ImageFont
 
-RED = (255, 0, 0)
-POLY = [(50, 50), (150, 50), (180, 120)]
 image = Image.new('RGB', (600, 400), (0, 0, 255))
-
 draw = ImageDraw.Draw(image)
 
-draw.line((0, 0, 600, 400), fill=(0, 255, 0), width=5)
-draw.line((600, 0, 0, 400), fill=RED, width=5)
-draw.rectangle((10, 10, 590, 390), outline=RED, width=10)
-draw.ellipse((10, 10, 590, 390), outline=RED, width=10)
-draw.polygon(POLY, outline='green', width=15)
-draw.text((100, 80), text='ghfdsg', fill=(0, 0, 0), font_size=40)
-
-
+draw.ellipse((470, -120, 800, 120), outline='yellow', fill='yellow')
+draw.text((150, 180), text="It's a sunny day", fill='yellow', font_size=40)
+draw.text((500, 380), text="Made by Sarge", fill='white', font_size=10)
 image.save('images/deep_blue.jpg')
-

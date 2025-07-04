45b781fb4ed3a24120dbf31696b1ac4ffd7e46fb
Sarge <a@b.ru>
2025-07-04 12:21:52 +0300

    Модуль random

--- main.py
+++ main.py
@@ -1,27 +1,54 @@
 # Встроеннные библиотеки
 # PyPI - Python Package Index (pypi.org)
-# from math import * # Так делать не стоит без причины
-# import math as m
-from math import pi, sqrt, radians, hypot
-from math import sin
+# sample (в отличие от choice) выбирает уникальные (без повторов)
+# s
+import random as r
+# r.seed()
+# print(r.random())
 
+abc = 'wqertyuiopasdfghjklzxcvbnm'
+# lst = list(abc) + ['1', '2'] + ['#', '$']
+N = 8
+num = '1234567890'
+spec = '!@#$%'
 
-print('Число Пи ', pi)
-print('Квадратный корень 4:', sqrt(4))
-print('Синус 30 градусов', round(sin(radians(30)), 2))
-print('Гипотенуза для 2 и 3', hypot(3, 2))
+abc = list(abc)
+num = list(num)
+spec = list(spec)
 
-# print('Число Пи ', m.pi)
-# print('Число Пи ', math.pi)
+temp = abc[:N - 3]
+temp.append(r.choice(abc).upper())
+temp.append(r.choice(num))
+temp.append(r.choice(spec))
+r.shuffle(temp)
+res = ''.join(temp)
 
-# print(help(m.cos))
+print(res)
 
-# res = sum([1, 2, 3])
-# print(res)
+# zara = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
 #
-# res1 = max(1, 3, 2)
-# print(res1)
+# for _ in range(10):
+#     print(r.choice(zara), r.choice(zara))
 #
-# res2 = min('a', 'bc', 'a')
-# print(res2)
+# d = {'a': 1,
+#      'b': 2,
+#      'c': 3,
+# }
+# keys = list(d.keys())
+#
+# key = r.choice(keys)
+# print(d[key])
 
+# lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#
+# for _ in range(5):
+#     print(r.sample(lst, k=5))
+
+# print(r.choice(['орёл', 'решка']))
+# print(r.choice('орёл'))
+
+# print(r.choice(lst))
+
+# for _ in range(10):
+#     r.randint(0, 10)
+#     print(r.randrange(0, 10, 2))

73929d158388631a051961a967b837feef43f2a8
Sarge <a@b.ru>
2025-07-07 11:34:57 +0300

    Docx Template2

--- main.py
+++ main.py
@@ -4,12 +4,26 @@
 # pip freeze > requirements.txt
 from docxtpl import DocxTemplate
 
+# # Загрузка шаблона
+# doc = DocxTemplate('docs/template.docx')
+#
+# # Данные для подстановки в шаблон
+# content = {
+#     'company': 'ООО "Монолит"',
+#     'employee': 'Петров Д.И.',
+#     'position': 'Менеджер',
+#     'date': '01/01/2025'
+# }
+#
+# doc.render(content)
+# doc.save('docs/about.docx')
+
 # Загрузка шаблона
-doc = DocxTemplate('docs/template.doc')
+doc = DocxTemplate('docs/template.docx')
 
 # Данные для подстановки в шаблон
 content = {
-    'company': 'ООО "Монолит"',
+    'company': 'OOO "Монолит"',
     'employee': 'Петров Д.И.',
     'position': 'Менеджер',
     'date': '01/01/2025'

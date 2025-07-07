7a257d37b65fbdcad9c3632a17e255394a4b7a97
Sarge <a@b.ru>
2025-07-07 15:32:19 +0300

    File дописать в конец

--- main.py
+++ main.py
@@ -5,17 +5,25 @@
 # w - write - файл открывается на запись(перезаписывается), если его нет, то создается
 # a - append - запись в коннец файла
 # r - read - чтение
+# print(*args, sep=' ', end='\n', file=None, flush=False)
 
-fo = open('info.txt', 'rt', encoding='utf-8')
+fo = open('info.txt', 'at', encoding='utf-8')
 
-text = fo.read(11)  # (5) - сколько байт читать
-fo.read(6)  # (5) - сколько байт читать
-# fo.read()  # (5) - сколько байт читать
-text += fo.read(7)
-print('Вот, что было в файле', end=': ')
-print(text)
+fo.write(' Хороший текст')
+# print('А вот это будет уже с новой строки')
 
 fo.close()
+
+# fo = open('info.txt', 'rt', encoding='utf-8')
+#
+# text = fo.read(11)  # (5) - сколько байт читать
+# fo.read(6)  # (5) - сколько байт читать
+# # fo.read()  # (5) - сколько байт читать
+# text += fo.read(7)
+# print('Вот, что было в файле', end=': ')
+# print(text)
+#
+# fo.close()
 
 # fo = open('info.txt', 'wt', encoding='utf-8')
 #

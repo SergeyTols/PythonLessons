bb06bd072c1db28cae9e350f6a9dfd52c00694cb
Sarge <a@b.ru>
2025-07-07 12:04:04 +0300

    Пустой Exel-файл

--- main.py
+++ main.py
@@ -1,46 +1,14 @@
 # Ctrl + Alt + o - убрать лишний импорт
-# Документы
-# pip install python-docx-template | pip install docxtpl
+# Документы (электронные таблицы)
+# Excel (openpyxl)  pip install openpyxl
 # pip freeze > requirements.txt
-from itertools import count
 
-from docxtpl import DocxTemplate
+# Пустой Exel-файл
+from openpyxl import Workbook
 
-# # Загрузка шаблона
-# doc = DocxTemplate('docs/template.docx')
-#
-# # Данные для подстановки в шаблон
-# content = {
-#     'company': 'ООО "Монолит"',
-#     'employee': 'Петров Д.И.',
-#     'position': 'Менеджер',
-#     'date': '01/01/2025'
-# }
-#
-# doc.render(content)
-# doc.save('docs/about.docx')
+wb = Workbook() # wb - Workbook
 
-# Загрузка шаблона
-doc = DocxTemplate('docs/template.docx')
+ws = wb.active
+ws.title = 'Отчет'
 
-# Данные для подстановки в шаблон
-content = [
-    {
-    'company': 'OOO "Монолит"',
-    'employee': 'Петров Д.И.',
-    'position': 'Менеджер',
-    'date': '01/01/2025'
-    },
-    {
-'company': 'OOO "Арсенал"',
-    'employee': 'Петров Д.И.',
-    'position': 'Менеджер',
-    'date': '01/01/2025'
-    }
-]
-
-count = 1
-for item in content:
-    doc.render(item)
-    doc.save(f'docs/about{count}.docx')
-    count +=1
+wb.save('docs/report.xlsx')

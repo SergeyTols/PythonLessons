00f7b8fde56e7e456918edbefedab6910c0591ee
Sarge <a@b.ru>
2025-07-07 11:47:55 +0300

    Docx Template Multy

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # Документы
 # pip install python-docx-template | pip install docxtpl
 # pip freeze > requirements.txt
+from itertools import count
+
 from docxtpl import DocxTemplate
 
 # # Загрузка шаблона
@@ -22,12 +24,23 @@ from docxtpl import DocxTemplate
 doc = DocxTemplate('docs/template.docx')
 
 # Данные для подстановки в шаблон
-content = {
+content = [
+    {
     'company': 'OOO "Монолит"',
     'employee': 'Петров Д.И.',
     'position': 'Менеджер',
     'date': '01/01/2025'
-}
+    },
+    {
+'company': 'OOO "Арсенал"',
+    'employee': 'Петров Д.И.',
+    'position': 'Менеджер',
+    'date': '01/01/2025'
+    }
+]
 
-doc.render(content)
-doc.save('docs/about.docx')
+count = 1
+for item in content:
+    doc.render(item)
+    doc.save(f'docs/about{count}.docx')
+    count +=1

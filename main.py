f3a471c42a85353c02c188c6f6506c9466461459
Sarge <a@b.ru>
2025-07-07 09:50:23 +0300

    H/W fixed3 good

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from PIL import Image, ImageDraw, ImageFont
 image = Image.new('RGB', (600, 400), (0, 0, 255))
 draw = ImageDraw.Draw(image)
 
+not_my_font = ImageFont.truetype(font='Fonts/Robloxian-UltraBold.ttf')
+
 draw.circle((600, 0), radius=150, fill='yellow')
 
 draw.text((100, 180), text="Это солнечный день", fill='yellow',
           font=ImageFont.truetype("Gabriola.ttf", 60))
 draw.text((500, 380), text="Made by Sarge", fill='white',
-          font=ImageFont.truetype(font=r'\PycharmProjects\PythonLessons\Fonts\Robloxian-UltraBold.ttf'),
+          font=not_my_font,
           font_size=20)
 image.save('images/deep_blue.jpg')

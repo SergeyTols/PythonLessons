fdfcac5d33e6b965a9810d281fcf27afecbe45f9
Sarge <a@b.ru>
2025-07-08 15:02:49 +0300

    Бутылки Pymorphy

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 # Библиотека pymorphy
 # pip install pymorphy3
 # pip install -U pymorphy3-dicts-ru
-import pymorphy3
+from pymorphy3 import MorphAnalyzer
 
-morph = pymorphy3.MorphAnalyzer()
+form = MorphAnalyzer().parse('бутылка')[0]
 
-print(morph.parse('Дмитрий'))
+for btl in reversed(range(99)):
+    print(f'В холодильнике {btl + 1} {form.make_agree_with_number(btl + 1).word} пива')
+    print('Возьмём одну и выпьем')
+    if btl % 10 == 1 and btl != 11:
+        remain = 'Осталась'
+    else:
+        remain = 'Осталось'
+    print(f'{remain} {btl} {form.make_agree_with_number(btl).word} пива.')
 
 

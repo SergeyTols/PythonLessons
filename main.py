dcb1d05df3dea39e342874dee783bbae324c7aca
Sarge <a@b.ru>
2025-07-08 14:38:17 +0300

    Clever dump (словарь)

--- main.py
+++ main.py
@@ -8,35 +8,63 @@
 # finally:
 #   <выполняется в любом случае>
 
+
+#   Практикум (обучаемый словарь)
+
+import pickle
+
+
+# инимальная версия, если файл dict.dat отсутствует
+voc = {
+    'стол': 'table',
+    'стул': 'chair',
+}
+
+
+# функция для распечатки словаря
+def print_voc():
+    print('Сейчас словарь содержит: ')
+    for k, v in voc.items():
+        print(k, '—', v)  # Alt + 0151
+
+
+
+# Загружаем словарь из файла
+try:
+    with open('dict.dat', 'rb') as dump_in:
+        voc = pickle.load(dump_in)
+except FileNotFoundError:
+    with open('dict.dat', 'wb') as dump_out:
+        pickle.dump(voc, dump_out)
+    print('Создан минимальный словарь: ')
+    print_voc()
+
 while True:
-    a = input('Введите первое число: ')
-    b = input('Введите второе число: ')
-    try:
-        result = int(a) / int(b)
-    except ZeroDivisionError:
-        print('На ноль делить нельзя!')
-    except ValueError:
-        print('Нужно вводить числа...')
-        print(f'А введено {a} и {b} :(')
-    else:
-        print(result)
+    temp = input('\nВведите слово для перевода или "#" для завершения: ')
+    word = temp.strip().lower()
+    if word == '#' or word == '№':
         break
+    if word in voc.keys():
+        translate = voc[word]
+        print(f'Слово "{word}" переводится как {translate}.\n')
+    else:
+        print(f'Значение слова {word} тсутствует в словаре.')
+        newkey = f'А как слово {word} переводится.\n'
+        newkey += 'Если ничего не вводите нажмите ENTER,\n '
+        newkey += 'или введите его здесь: '
+        new_word = input(newkey)
 
-# if a.isdigit() and b.isdigit():
-#     else:
-#         print(int(a) / int(b))
+        if new_word != '' or len(new_word) > 2:
+            voc[word] = new_word
+            print(f'Слово {word} с переводом {new_word} внесено в словарь')
+        else:
+            print('Ничего не введено или слишком короткое слово')
+            continue
 
 
-# lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-#
-# loop = True
-# while loop:
-#     try:
-#         index = int(input('Введите индекс: '))
-#         if not index < len(lst):
-#             raise ValueError('Введенный индекс вне диапазона')
-#         print(f'Число по индексу {index}: {lst[index]}')
-#     except ValueError as exp:
-#         print('Надо быть внимательнее:', exp)
-#     else:
-#         loop = False
+# Сохранить словарь
+with open('dict.dat', 'wb') as dump_out:
+    pickle.dump(voc, dump_out)
+
+print('До новых встреч!!!')
+

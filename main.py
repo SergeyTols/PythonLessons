1e6d038e7c8ae6e45b9079096e45341a8843f445
Sarge <a@b.ru>
2025-07-10 14:10:11 +0300

    Special (Magic Methods) point

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # Duck type - утиная типизация (прочитать)
-#
+# Шпоргалка по всем методам в пайтон
 #
 # ООП (magic methods)
 # method override; operator overloading
 #
 #
 #
+from math import hypot
 
 class Point:
     def __init__(self, x=0, y=0):
@@ -18,8 +19,22 @@ class Point:
     def __repr__(self):
         return f'<List of Point: ({self.x}, {self.y})>'
 
-p = Point()
-po = [Point(), Point()]
-print(p)
-print(po)
+    def __sub__(self, other):
+        # return Point(self.x - other.x, self.y - other.y)
+        return Point(abs(self.x - other.x), abs(self.y - other.y))
+
+    def __add__(self, other):
+        # hypot = (abs(self.x - other.x), abs(self.y - other.y))
+        # abs(self.x - other.x), abs(self.y - other.y)
+        return hypot((self.x - other.x), (self.y - other.y))
+
+
+# a = round(hypot(5, 2))
+p = Point(5, 4)
+po = Point(10, 2)
+
+
+print(p - po)
+
+
 

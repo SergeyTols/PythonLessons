f4bb99658155aa36439c56fd81cdb6837e80196b
Sarge <a@b.ru>
2025-07-11 12:16:42 +0300

    NET Intro and CLI

--- main.py
+++ main.py
@@ -52,5 +52,16 @@
 # python main.py -  запуск скрипта/программы из под командной строки
 #
 #
+import sys
 
-print('hello world')
+
+print('Я', sys.argv[0], 'и мой аргумент', sys.argv[1])
+
+if len(sys.argv) >= 2:
+    match sys.argv[1]:
+        case 'p':
+            print('Привет')
+        case 'g':
+            print('Пока')
+        case _:
+            print('Не понял')

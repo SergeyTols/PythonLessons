1cf4c19e6c53005b0c8d22815a578f379ab34140
Sarge <a@b.ru>
2025-07-14 14:26:52 +0300

    CSV Dict and Quoting

--- main.py
+++ main.py
@@ -2,21 +2,29 @@
 
 import csv
 
+from urllib3.filepost import writer
+
 # with open('people.csv', 'r', encoding='utf-8') as f:
 #     dict_reader = csv.DictReader(f)
 #     for row in dict_reader:
 #         print(f'{row['name']} живет в городе {row['city']}')
 
-field_names = ['name','age','city']
-data = {
-    'name' : 'Борька',
-    'age' : 27,
-    'city' : 'Москва'
-}
-
-with open('file.csv', 'w', newline='', encoding='utf-8') as f:
-    writer = csv.DictWriter(f, fieldnames=field_names)
+# Режимы квотирования
+data = ['name', 25, 'town']
+with open('sample.csv', 'w', newline='', encoding='utf-8') as f:
+    writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
     writer.writerow(data)
+
+# field_names = ['name','age','city']
+# data = {
+#     'name' : 'Борька',
+#     'age' : 27,
+#     'city' : 'Москва'
+# }
+#
+# with open('file.csv', 'w', newline='', encoding='utf-8') as f:
+#     writer = csv.DictWriter(f, fieldnames=field_names)
+#     writer.writerow(data)
 # data = [
 #     ['name', 'age', 'city'],
 #     ['Борька', '25', 'Воронеж'],

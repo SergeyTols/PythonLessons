486d6549187c3a7ffcb44e79e668bc7dce56310f
Sarge <a@b.ru>
2025-07-14 14:42:45 +0300

    Zip Files

--- main.py
+++ main.py
@@ -1,41 +1,25 @@
-# CSV-файлы
+# Zip
 
-import csv
+from zipfile import ZipFile
+import os
 
+# csv_files = [f for f in os.listdir() if f.endswith('.csv')]
+# # print(csv_files)
+# with ZipFile('archive.zip', 'w') as myzip:
+#     for file in csv_files:
+#         myzip.write(file)
+#         os.remove(file)
 
-# with open('people.csv', 'r', encoding='utf-8') as f:
-#     dict_reader = csv.DictReader(f)
-#     for row in dict_reader:
-#         print(f'{row['name']} живет в городе {row['city']}')
+# получить список
+with ZipFile('archive.zip', 'r') as zip_obj:
+    print(zip_obj.namelist())
 
-# Режимы квотирования
-data = ['name', 25, 'town']
-with open('sample.csv', 'w', newline='', encoding='utf-8') as f:
-    writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
-    writer.writerow(data)
-
-# field_names = ['name','age','city']
-# data = {
-#     'name' : 'Борька',
-#     'age' : 27,
-#     'city' : 'Москва'
-# }
+# распаковать определенный
+# files_to_extract = ['people.csv', 'file.csv']
 #
-# with open('file.csv', 'w', newline='', encoding='utf-8') as f:
-#     writer = csv.DictWriter(f, fieldnames=field_names)
-#     writer.writerow(data)
-# data = [
-#     ['name', 'age', 'city'],
-#     ['Борька', '25', 'Воронеж'],
-#     ['Влад', '75', 'Тверь'],
-#     ['Глеб', '35', 'Краснодар'],
-# ]
-#
-# with open('people.csv', 'r', encoding='utf-8') as f:
-#     reader = csv.reader(f, delimiter=',', quotechar='"')
-#     for row in reader:
-#         print(row)
-#
-# with open('employee.csv', 'w', newline='', encoding='utf-8') as f:
-#     writer = csv.writer(f)
-#     writer.writerows(data)
+# with ZipFile('archive.zip', 'r') as zip_obj:
+#     zip_obj.extractall(members=files_to_extract)
+
+# распаковать все
+with ZipFile('archive.zip', 'r') as zip_obj:
+     zip_obj.extractall()

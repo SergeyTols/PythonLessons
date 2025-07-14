409277315cc5e7e2cdb33619526d7473a33c432e
Sarge <a@b.ru>
2025-07-14 15:23:06 +0300

    JSON Read

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ import json
 
 
 with open('dogs.json', 'rt') as d:
-    data = json.load(d)
+    # data = json.load(d) # напрямую из файла
+    temp = d.read() # читаем как строку
+    data = json.loads(temp) # строковое представление JSON
+    print(data)
 
-for k, v in data.items():
-    if type(v) == list:
-        print(f'{k}: {', '.join(v)}')
-    else:
-        print(f'{k}: {v}')
+# for k, v in data.items():
+#     if type(v) == list:
+#         print(f'{k}: {', '.join(v)}')
+#     else:
+#         print(f'{k}: {v}')
 
 # print(json.dumps(data, indent=4))
